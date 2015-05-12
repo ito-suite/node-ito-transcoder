2ac6e8ff20d91af8efce45f171e889f96cc6a4eb
Denjell <denjell@das-gaengeviertel.info>
2015-05-12 16:38:01 +0200

    added video conCAT

--- bin/lib/videos2video.sh
+++ bin/lib/videos2video.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# todo: write this with melt
+for file in $@
+    do
+        string=$(echo ${string} "${file}")
+    done
 
-# gives a list of input files and the last is the output file
-
-# like so:
-
-# melt $1 $2 $3 $4 -consumer avformat:test2.mp4 height=720 width=1080
+melt ${string} -consumer avformat:output.mp4
